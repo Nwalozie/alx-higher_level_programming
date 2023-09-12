b8db5e1edfe8686da1c2d1ce6da742757aa8e88f
Nwalozie <iyekcnwalozie@gmail.com>
2023-09-12 05:42:30 +0100

    Updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const argc = process.argv.length;
-
-if (argc > 2) {
-  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
-} else {
+if (process.argv[2] === undefined) {
   console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
