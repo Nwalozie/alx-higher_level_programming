eaae3290a394f071e722340720a978c5d4c2d842
Nwalozie <iyekcnwalozie@gmail.com>
2023-09-12 05:25:35 +0100

    JavaScript - Warm Up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
+const x = process.argv[2];
 
-const args = process.argv.slice(2);
-const size = parseInt(args[0]);
-
-if (!isNaN(size)) {
-  if (size > 0) {
-    for (let i = 0; i < size; i++) {
-      let row = '';
-      for (let j = 0; j < size; j++) {
-        row += 'X';
-      }
-      console.log(row);
-    }
-  } else {
-    console.log('Size must be a positive integer.');
-  }
-} else {
+if (!parseInt(x)) {
   console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    let y = 0;
+    let myVar = '';
+
+    while (y < x) {
+      myVar = myVar + 'X';
+      y++;
+    }
+    console.log(myVar);
+  }
 }
